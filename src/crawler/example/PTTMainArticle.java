e4ddcab56d95eaeeae30064dddd3d8967a47b909
ohce0 <gauldoth8@gmail.com>
2017-04-09 11:45:32 +0800

    just a test

---
+++ src/crawler/example/PTTMainArticle.java
@@ -0,0 +1,43 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.jsoup.select.Elements;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PTTMainArticle {
+	// commit test  second!
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1491698530.A.0FC.html";
+        Elements ele = CrawlerPack.start()
+
+                // 參數設定
+                .addCookie("over18", "1")    // 設定cookie
+                //.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+
+                // 選擇資料格式 (三選一)
+                //.getFromJson(uri)
+                .getFromHtml(uri)
+                //.getFromXml(uri)
+
+
+                // 這兒開始是 Jsoup Document 物件操作
+                .select("#main-content");
+
+        ele.select("span, div").remove();
+
+		System.out.println(ele.text().replaceAll("<.*>",""));
+	}
+}
